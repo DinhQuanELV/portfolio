5d007638dcbf85c2306c5f67e49acc3c10706a98
Dinh Quan <quanvonguyendinh@gmail.com>
2025-03-26 20:57:10 +0700

    set up components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import './App.css';
+import AboutMe from './components/AboutMe';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Home from './components/Home';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
 
 function App() {
   return (
     <>
-      <h1>Portfolio</h1>
+      <Header />
+      <Home />
+      <AboutMe />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
     </>
   );
 }
