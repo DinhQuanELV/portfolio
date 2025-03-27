e8b85263249c3c3cd5312b2323be90fd4d2af697
Dinh Quan <quanvonguyendinh@gmail.com>
2025-03-27 13:58:59 +0700

    handle hooks dark mode

---
+++ src/hooks/useDarkMode.jsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react';
+
+export const useDarkMode = () => {
+  const [darkMode, setDarkMode] = useState(() => {
+    return (
+      typeof window !== 'undefined' && localStorage.getItem('theme') === 'dark'
+    );
+  });
+
+  useEffect(() => {
+    document.documentElement.classList.toggle('dark', darkMode);
+    localStorage.setItem('theme', darkMode ? 'dark' : 'light');
+  }, [darkMode]);
+
+  const handleToggleTheme = () => {
+    setDarkMode((prev) => !prev);
+  };
+
+  return { darkMode, handleToggleTheme };
+};
